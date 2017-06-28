b38b4a87887ddf192ac451712723dfb71fde066e
Danielle Frappier <daniellerichard1@gmail.com>
2017-06-28 19:31:27 -0400

    first comit

---
+++ js/script.js
@@ -0,0 +1,147 @@
+/*  Preloader */
+$(window).load(function () {
+		
+    $('#preloader').delay(250).fadeOut('slow', function () {
+        $('.profile-page, .resume-page, .contact-page, .portfolio-page').hide();
+    });
+});
+
+$(document).ready(function () {
+
+    'use strict';
+
+/* Homepage */
+    function homepageResponsive() {
+        var windowsWidth = $(window).width(),
+            windowsHeight = $(window).height();
+
+        if (windowsWidth > windowsHeight) {
+
+            $('.introduction , .menu').css({
+                width: '50%',
+                height: '100%'
+            });
+
+        } else {
+
+            $('.introduction , .menu').css({
+                width: '100%',
+                height: '50%'
+            });
+
+        }
+        var introWidth = $('.introduction').width(),
+            introHeight = $('.introduction').height(),
+            bgImage = $('.introduction').find('img');
+
+        if (introWidth > introHeight) {
+
+            bgImage.css({
+                width: '100%',
+                height: 'auto'
+            });
+
+        } else {
+
+            bgImage.css({
+                width: 'auto',
+                height: '100%'
+            });
+
+        }
+
+    }
+
+    $(window).on('load resize', homepageResponsive);
+
+    /* Menu */
+    $('.menu > div').on('click', function () {
+
+        var introWidth = $('.introduction').width(),
+            menuWidth = $('.menu').width();
+
+        $('.introduction').animate({
+            left: '-' + introWidth
+        }, 1000, 'easeOutQuart');
+        $('.menu').animate({
+            left: menuWidth
+        }, 1000, 'easeOutQuart', function () {
+            $('.home-page').css({
+                visibility: 'hidden'
+            });
+        });
+
+    });
+
+    $('.menu div.profile-btn').on('click', function () {
+        $('.profile-page').fadeIn(1200);
+    });
+
+    $('.menu div.resume-btn').on('click', function () {
+        $('.resume-page').fadeIn(1200);
+    });
+
+    $('.menu div.portfolio-btn').on('click', function () {
+        $('.portfolio-page').fadeIn(1200);
+    });
+
+    $('.menu div.contact-btn').on('click', function () {
+        $('.contact-page').fadeIn(1200);
+    });
+
+    $('.close-btn').on('click', function () {
+        $('.home-page').css({
+            visibility: 'visible'
+        });
+        $('.introduction, .menu').animate({
+            left: 0
+        }, 1000, 'easeOutQuart');
+        $('.profile-page, .resume-page, .portfolio-page, .contact-page').fadeOut(800);
+    });
+
+    /* Services Height */
+    function maximizeHeight() {
+
+        var minHeight = 0;
+
+        $('.services').each(function () {
+
+            var maxHeight = $(this).height();
+
+            if (maxHeight > minHeight) {
+                minHeight = maxHeight;
+            }
+
+        });
+
+        $('.services').height(minHeight);
+    }
+
+    maximizeHeight();
+
+    $(window).on('resize', maximizeHeight);
+
+    $(function () {
+
+        // EasyPieChart Init
+        $('.skill').easyPieChart({
+            barColor:   '#158a78',
+            trackColor: '#c6c6c6',
+            scaleColor: '#c6c6c6',
+            scaleLength: 8,
+            lineWidth: 8,
+            size: 150,
+            lineCap: 'butt'
+        });
+
+    });
+
+    /* PopUp */
+    $(function () {
+        $('.show-popup').popup({
+            keepInlineChanges: true,
+            speed: 500
+        });
+    });
+
+});
